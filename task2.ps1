817c687855275cf83c73eb41283bd829964609d7
Anna Vasileva <miss.niakris@gmail.com>
2023-07-25 00:14:42 +0300

    done

---
+++ task2.ps1
@@ -0,0 +1,36 @@
+﻿    param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        $CsvFile
+    )
+
+    $accounts = Import-Csv -path $CsvFile -Delimiter ','
+    $TextInfo = (Get-Culture).TextInfo
+    
+    $emails = @()
+    $accounts_new = @()
+
+    foreach($line in $accounts)
+    {
+        $email = ($line.name.Split(' ')[0][0]+$line.name.Split(' ')[1]).ToLower()
+        $emails+=($email)
+
+        $newLine = [PSCustomObject]@{
+            id = $line.id
+            location_id=$line.location_id
+            Name = $TextInfo.ToTitleCase($line.name)
+            title=$line.title
+            email = $email
+            department=$line.department
+        }
+        $accounts_new+=$newline        
+    }
+
+    foreach($line in $accounts_new){
+        if (($emails -match $line.email) -gt 1){
+            $line.email += $line.location_id+'@abc.com'
+        }
+    }
+
+    $path = "$((Get-ChildItem $CsvFile).Directory)\accounts_new.csv"
+     $accounts_new |Export-Csv -Path $path -NoTypeInformation -Delimiter ','
